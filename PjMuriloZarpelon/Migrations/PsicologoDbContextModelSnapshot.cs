27912a62c1dd46855dc93d85a7319e5e3024916c
Murilo Zarpelon <404230@unoesc.lan>
2023-08-24 21:23:40 -0300

    OK

---
+++ PjMuriloZarpelon/Migrations/PsicologoDbContextModelSnapshot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PjMuriloZarpelon.Data;
+
+namespace PjMuriloZarpelon.Migrations
+{
+
+}
